234df049a96c7d10dd90e94532a680348d8c3de4
samer <samer@king.com>
2020-03-23 15:28:47 +0100

    Point2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
+var patientsRouter = require('./routes/patients')
 
 var app = express();
 
@@ -18,5 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/patients', patientsRouter)
+
 
 module.exports = app;
