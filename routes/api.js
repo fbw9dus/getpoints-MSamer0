003f8bd93aedd7c402f29fa192065f78f2d92e4c
samer <samer@king.com>
2020-03-23 11:52:39 +0100

    Point

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  var sam = 'Why so Serious'
-  res.status(200).send(sam);
+  var user = [{user:"fake", password:"fake"}]
+  res.status(200).json(user)
 });
 
 module.exports = router;
-
